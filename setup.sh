8d3c13fdbb11766960d1805cd38c607195515700
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2023-08-30 17:32:15 -0400

    grab OS from first-of-month release file

--- setup.sh
+++ setup.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
+dependencies=(
+  mkdir wget date
+  sudo
+)
 
+for dependency in "${dependencies[@]}" ; do
+  if ! which $dependency 2>/dev/null >/dev/null ; then
+    echo "Cannot find a required tool '$dependency', please install $dependency and try again."
+    exit 1
+  fi
+done
+
+root_dir="$PWD/container_root"
+
+if ! [ -e "$root_dir" ] || ! [ -e "$root_dir/root" ]; then
+  echo "Creating an Arch Linux container at $root_dir"
+  sudo mkdir -p "$root_dir"
+
+  dl_url="http://mirror.adectra.com/archlinux/iso/$(date +"%Y.%m.01")/archlinux-bootstrap-x86_64.tar.gz" 
+  echo "Downloading an Arch Linux root filesystem from $dl_url"
+
+  wget -qO- "$dl_url" | sudo tar xvz -C "$root_dir" --strip-components=1
+
+fi
 
 
 
